f39ac70098bba454db8dd958a88a6e631a6516d8
David Wardlaw <Dwardlaw@imeta.co.uk>
2018-09-05 13:17:38 +0100

    Initial Benchmarking project to show the nuget package and opening notepad

---
+++ BenchMarkingExploration/MyFirstBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using System.Diagnostics;
+
+namespace BenchMarkingExploration
+{
+    [SimpleJob(launchCount: 1, warmupCount: 3, targetCount: 4, invocationCount: 5)]
+    //[ShortRunJob]
+    public class MyFirstBenchmark
+    {
+        [GlobalSetup()]
+        public void GlobalSetup()
+        {
+        }
+
+        [GlobalCleanup()]
+        public void GlobalCleanup()
+        {
+            Process[] proc = Process.GetProcessesByName("notepad");
+            int i = 0;
+            foreach (var item in proc)
+            {
+                proc[i].Kill();
+                i++;
+            }
+            //Console.ReadLine();
+        }
+
+        [Benchmark(Description = "Open Notepad ++")]
+        public void OpenNotePad()
+        {
+            ProcessStartInfo NotePad = new ProcessStartInfo();
+            NotePad.FileName = "notepad.exe";
+            Process.Start(NotePad);
+
+           }
+
+    }
+}
---
+++ BenchMarkingExploration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+
+namespace BenchMarkingExploration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MyFirstBenchmark>();
+            Console.ReadKey();
+        }
+    }
+}
